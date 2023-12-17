4d4a59f2c06a01733bb946b62262fe058d59d8d1
espisepi <sepinaco@icloud.com>
2023-12-17 21:10:52 +0100

    ThreeWorld TS helloWorld

--- src/app/services/threejs/threeWorld.service.ts
+++ src/app/services/threejs/threeWorld.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
+import { ThreeWorld } from '../../../threejs/world/ThreeWorld';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ThreeWorldService {
+  world: ThreeWorld | null = null;
+
   constructor() {}
 
   initWorld() {
     console.log('InitWorld!');
+    this.world = new ThreeWorld();
   }
 }
---
+++ src/threejs/world/ThreeWorld.ts
@@ -0,0 +1,5 @@
+export class ThreeWorld {
+  constructor() {
+    console.log('Hello ThreeWorld TS');
+  }
+}
