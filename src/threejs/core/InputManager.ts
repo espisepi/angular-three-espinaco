1c0406281cf00a382745ed5fe44098c07d928fac
espisepi <sepinaco@icloud.com>
2023-12-18 20:04:36 +0100

    pointerlock false

--- src/threejs/core/InputManager.ts
+++ src/threejs/core/InputManager.ts
@@ -24,7 +24,7 @@ export class InputManager implements IUpdatable {
 
   constructor(world: World, domElement: HTMLElement) {
     this.world = world;
-    this.pointerLock = true; // world.params.Pointer_Lock ;
+    this.pointerLock = false; // world.params.Pointer_Lock ;
     this.domElement = domElement || document.body;
     this.isLocked = false;
 
